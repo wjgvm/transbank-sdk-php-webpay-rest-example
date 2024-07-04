66c7d6c7adda204eadf2a435e96095c68216d7a4
William <williamvasquzmillan@gmail.com>
2024-07-04 02:32:29 -0400

    Update use WebpayPlusController improve processes route

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,8 @@
 use Illuminate\Support\Facades\Route;
 
 use App\Http\Controllers\HomeController;
+use App\Http\Controllers\WebpayPlusController;
+
 // Route::get('/', function () {
 //     return view('welcome');
 // });
@@ -14,15 +16,15 @@ Route::get('/webpayplus/create', function () {
     return view('webpayplus/create');
 });
 
-Route::post('/webpayplus/create/', 'WebpayPlusController@createdTransaction');
+Route::post('/webpayplus/create/', [WebpayPlusController::class,'createdTransaction']);
 
-Route::any('/webpayplus/returnUrl', 'WebpayPlusController@commitTransaction');
+Route::any('/webpayplus/returnUrl', [WebpayPlusController::class,'commitTransaction']);
 
-Route::get('/webpayplus/refund', 'WebpayPlusController@showRefund');
-Route::post('/webpayplus/refund', 'WebpayPlusController@refundTransaction');
+Route::get('/webpayplus/refund', [WebpayPlusController::class,'showRefund']);
+Route::post('/webpayplus/refund', [WebpayPlusController::class,'refundTransaction']);
 
-Route::get('/webpayplus/transactionStatus', 'WebpayPlusController@showGetStatus');
-Route::post('/webpayplus/transactionStatus', 'WebpayPlusController@getTransactionStatus');
+Route::get('/webpayplus/transactionStatus', [WebpayPlusController::class,'showGetStatus']);
+Route::post('/webpayplus/transactionStatus', [WebpayPlusController::class,'getTransactionStatus']);
 
 # Webpay Plus Diferido
 Route::get('/webpayplus/diferido/create', function () {
